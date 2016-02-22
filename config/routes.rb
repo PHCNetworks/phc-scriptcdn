c2d3546224237abfd67cf021263a16052760e866
Brad Potts <bradley.j.potts@gmail.com>
2016-02-22 04:42:50 +0000

    Updated routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Phcscriptcdn::Engine.routes.draw do
-  namespace :scriptcdn do
-    resources :scripturls
-  end
-  namespace :scriptcdn do
-    resources :scriptversions
-  end
-  namespace :scriptcdn do
-    resources :scripts
-  end
+
+	namespace :scriptcdn do
+		resources :scripts do
+			resources :scripturls
+		end
+		resources :scriptversions
+	end
+
 end
