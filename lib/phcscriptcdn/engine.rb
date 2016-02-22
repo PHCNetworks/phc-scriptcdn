333528a1ccd471bf24eed253a8f83fae88fedb7c
Brad Potts <bradley.j.potts@gmail.com>
2016-02-22 02:39:04 +0000

    Updated to engine config re: respec testing.

--- lib/phcscriptcdn/engine.rb
+++ lib/phcscriptcdn/engine.rb
@@ -1,5 +1,17 @@
 module Phcscriptcdn
-  class Engine < ::Rails::Engine
-    isolate_namespace Phcscriptcdn
-  end
+	class Engine < ::Rails::Engine
+		isolate_namespace Phcscriptcdn
+
+		config.generators do |g|
+			g.test_framework :rspec,
+			fixtures: true,
+			view_specs: false,
+			helper_specs: false,
+			routing_specs: false,
+			controller_specs: true,
+			request_specs: false
+			g.fixture_replacement :factory_girl, dir: "spec/factories"
+		end
+
+	end
 end
