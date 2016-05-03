8415d2131556bd7bdaac94e29aaf7820e2565a8d
BradPotts <brad.potts@phcnetworks.net>
2016-05-03 01:47:10 +0000

    Fixes to db migration file scriptversions.

--- db/migrate/20160222030223_create_phcscriptcdn_scriptcdn_scriptversions.rb
+++ db/migrate/20160222030223_create_phcscriptcdn_scriptcdn_scriptversions.rb
@@ -2,7 +2,7 @@ class CreatePhcscriptcdnScriptcdnScriptversions < ActiveRecord::Migration
 	def change
 		create_table :phcscriptcdn_scriptcdn_scriptversions do |t|
 
-			T.string :scrptversionpakname
+			t.string :scrptversionpakname
 			t.string :scrptversion
 
 			t.timestamps null: false
