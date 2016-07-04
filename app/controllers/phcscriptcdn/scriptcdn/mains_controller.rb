6829977cb8bf60f57d678a741a68260e5c7a35a7
Brad Potts <developers@phcnetworks.net>
2016-07-04 02:26:15 +0000

    updated mains controller.

--- app/controllers/phcscriptcdn/scriptcdn/mains_controller.rb
+++ app/controllers/phcscriptcdn/scriptcdn/mains_controller.rb
@@ -1,62 +1,62 @@
 require_dependency "phcscriptcdn/application_controller"
 
 module Phcscriptcdn
-  class Scriptcdn::MainsController < ApplicationController
-    before_action :set_scriptcdn_main, only: [:show, :edit, :update, :destroy]
+	class Scriptcdn::MainsController < ApplicationController
 
-    # GET /scriptcdn/mains
-    def index
-      @scriptcdn_mains = Scriptcdn::Main.all
-    end
+		# Security & Action Filters
+		layout '/layouts/phcscriptcdn/application.html.erb'
+		before_action :set_scriptcdn_main, only: [:edit, :update, :destroy]
 
-    # GET /scriptcdn/mains/1
-    def show
-    end
+		# Script CDN Backend Index
+		def index
+			@scriptcdn_mains = Scriptcdn::Main.all
+		end
 
-    # GET /scriptcdn/mains/new
-    def new
-      @scriptcdn_main = Scriptcdn::Main.new
-    end
+		# New Script Listing
+		def new
+			@scriptcdn_main = Scriptcdn::Main.new
+		end
 
-    # GET /scriptcdn/mains/1/edit
-    def edit
-    end
+		# Edit Script Listing
+		def edit
+		end
 
-    # POST /scriptcdn/mains
-    def create
-      @scriptcdn_main = Scriptcdn::Main.new(scriptcdn_main_params)
+		# POST Script CDN
+		def create
+			@scriptcdn_main = Scriptcdn::Main.new(scriptcdn_main_params)
+			if @scriptcdn_main.save
+				redirect_to scriptcdn_mains_path, notice: 'Script was successfully created.'
+				else
+					render :new
+			end
+		end
 
-      if @scriptcdn_main.save
-        redirect_to @scriptcdn_main, notice: 'Main was successfully created.'
-      else
-        render :new
-      end
-    end
+		# PATCH/PUT Script
+		def update
+			if @scriptcdn_main.update(scriptcdn_main_params)
+				redirect_to scriptcdn_mains_path, notice: 'Script was successfully updated.'
+				else
+					render :edit
+			end
+		end
 
-    # PATCH/PUT /scriptcdn/mains/1
-    def update
-      if @scriptcdn_main.update(scriptcdn_main_params)
-        redirect_to @scriptcdn_main, notice: 'Main was successfully updated.'
-      else
-        render :edit
-      end
-    end
+		# DELETE Script
+		def destroy
+			@scriptcdn_main.destroy
+			redirect_to scriptcdn_mains_path, notice: 'Main was successfully destroyed.'
+		end
 
-    # DELETE /scriptcdn/mains/1
-    def destroy
-      @scriptcdn_main.destroy
-      redirect_to scriptcdn_mains_url, notice: 'Main was successfully destroyed.'
-    end
+		private
 
-    private
-      # Use callbacks to share common setup or constraints between actions.
-      def set_scriptcdn_main
-        @scriptcdn_main = Scriptcdn::Main.find(params[:id])
-      end
+		# Common Callbacks
+		def set_scriptcdn_main
+			@scriptcdn_main = Scriptcdn::Main.find(params[:id])
+		end
 
-      # Only allow a trusted parameter "white list" through.
-      def scriptcdn_main_params
-        params.require(:scriptcdn_main).permit(:scriptname)
-      end
-  end
+		# Whitelist
+		def scriptcdn_main_params
+			params.require(:scriptcdn_main).permit(:scriptname)
+		end
+
+	end
 end
