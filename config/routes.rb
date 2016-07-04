63a97e0c9026b16c38d3ca62c63288e666a5c53e
Brad Potts <developers@phcnetworks.net>
2016-07-04 02:34:49 +0000

    fixes to routes and updated testapp.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,34 @@
 Phcscriptcdn::Engine.routes.draw do
 
+	# CDN Frontend
+	namespace :frontend do
+
+		# Frontend Routes
+		resources :cdnpages
+
+	end
+
 	# Script CDN Listing System
 	namespace :scriptcdn do
-		resources :scripts
-		resources :scriptversions do
-			resources :scripturls
+
+		# Main Script Module
+		resources :mains do
+			resources :authors
+			resources :urls
+			resources :versions
+			resources :informations
 		end
+
+	end
+	
+	# Application API
+	namespace :api, :path => "", :constraints => {:subdomain => "api"} do
+
+		# Routes for API
+		resources :mains, defaults: {format: 'json'}
+		resources :informations, defaults: {format: 'json'}
+		resources :versions, defaults: {format: 'json'}
+
 	end
 
 end
---
+++ spec/testapp/db/schema.rb
@@ -0,0 +1,70 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20160704023112) do
+
+  create_table "phcscriptcdn_scriptcdn_authors", force: :cascade do |t|
+    t.string   "authorfirstname"
+    t.string   "authorlastname"
+    t.string   "authorwebsite"
+    t.string   "authorgithub"
+    t.string   "authortwitter"
+    t.integer  "main_id"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.index ["main_id"], name: "index_phcscriptcdn_scriptcdn_authors_on_main_id"
+  end
+
+  create_table "phcscriptcdn_scriptcdn_information", force: :cascade do |t|
+    t.string   "scripttitle"
+    t.text     "scriptdescription"
+    t.string   "scriptcategory"
+    t.string   "scriptwebsite"
+    t.string   "scripttwitter"
+    t.string   "scriptgithub"
+    t.date     "scriptinitialrelease"
+    t.string   "scriptlicence"
+    t.string   "scriptplatform"
+    t.string   "scriptstatus"
+    t.integer  "main_id"
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
+    t.index ["main_id"], name: "index_phcscriptcdn_scriptcdn_information_on_main_id"
+  end
+
+  create_table "phcscriptcdn_scriptcdn_mains", force: :cascade do |t|
+    t.string   "scriptname"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "phcscriptcdn_scriptcdn_urls", force: :cascade do |t|
+    t.string   "scripturl"
+    t.string   "scripturlext"
+    t.integer  "main_id"
+    t.string   "user_id"
+    t.string   "membership_id"
+    t.string   "oganization_id"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+    t.index ["main_id"], name: "index_phcscriptcdn_scriptcdn_urls_on_main_id"
+  end
+
+  create_table "phcscriptcdn_scriptcdn_versions", force: :cascade do |t|
+    t.string   "versionnumber"
+    t.integer  "main_id"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.index ["main_id"], name: "index_phcscriptcdn_scriptcdn_versions_on_main_id"
+  end
+
+end
